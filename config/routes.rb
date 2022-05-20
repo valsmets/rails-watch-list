8a3feef1c0aac64f7e57892c2421e948746bd622
valsmets <valsmets@hotmail.com>
2022-05-20 15:49:54 +0200

    je fais un beau meassage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
+  resources :lists, only: [ :show, :new, :create, :index, :destroy ]do
+    resources :bookmarks, only: [ :new, :create ] # /lists/1/bookmarks/new
+  end
+
+  resources :bookmarks, only: [ :destroy ]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
